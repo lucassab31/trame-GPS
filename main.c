ac45618671fd787895e58ed6a2d05da3466ff7ec
Lucas Sabathé <luqui31@gmail.com>
2020-05-12 09:38:54 +0200

    test commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
-
+// hello les bitch
 
 void main() {
     char trame[] = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76";
